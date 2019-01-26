d3119a29117655a72bf8bbff3fad3ac84c52f9d9
MyNameIsCat4Ever <47056539+MyNameIsCat4Ever@users.noreply.github.com>
2019-01-26 17:39:47 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("538727763373457440")
+setInterval(function() {
+channel.send(`..spam cat spam cat`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
